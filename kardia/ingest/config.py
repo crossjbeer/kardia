47cefc70472b4e988d3b5d525142142a5d86230f
Crossland Beer <crossjbeer@gmail.com>
2026-03-25 20:04:27 -0400

    Passing embedding parameters from the main config

--- kardia/ingest/config.py
+++ kardia/ingest/config.py
@@ -7,6 +7,6 @@ from kardia.config import Config
 class IngestionConfig:
     database_url: str = Config.POSTGRES_URL
     embedding_model_name: str = Config.EMBEDDING_MODEL
-    chunk_size: int = 512
-    chunk_overlap: int = 64
+    chunk_size: int = Config.MAX_CHUNK_SIZE_CHARS
+    chunk_overlap: int = Config.CHUNK_OVERLAP_CHARS
     lore_dir: str = Config.LORE_DIR
